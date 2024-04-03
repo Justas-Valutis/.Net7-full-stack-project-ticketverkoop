166c1ccd37ba0767c593ea30280cb7b8399794bc
Justas Valutis <r0940172@student.vives.be>
2024-04-03 18:32:47 +0200

    code cleanup

--- TicketVerkoop.Repositories/StadiumDAO.cs
+++ TicketVerkoop.Repositories/StadiumDAO.cs
@@ -6,50 +6,50 @@ using TicketVerkoop.Repositories.Interfaces;
 namespace TicketVerkoop.Repositories;
 
 public class StadiumDAO : IDAO<Stadium>
-{
+    {
     private readonly SoccerDbContext _dbContext;
 
     public StadiumDAO()
-    {
+        {
         _dbContext = new SoccerDbContext();
-    }
-    public async Task<IEnumerable<Stadium>?> GetAll()
-    {
-        try
-        {
-            return await _dbContext.Stadia.ToListAsync();
         }
-        catch (Exception ex)
+    public async Task<IEnumerable<Stadium>?> GetAll()
         {
+        try
+            {
+            return await _dbContext.Stadia.ToListAsync();
+            }
+        catch (Exception ex)
+            {
             Console.WriteLine(ex.ToString());
             throw new Exception("ERROR IN DAO" + ex.Message);
+            }
         }
-    }
 
     public Task<Stadium?> FindById(int Id)
-    {
+        {
         throw new NotImplementedException();
-    }
+        }
 
     public Task Add(Stadium entity)
-    {
+        {
         throw new NotImplementedException();
-    }
+        }
 
     public Task Delete(Stadium entity)
-    {
+        {
         throw new NotImplementedException();
-    }
+        }
 
 
 
     public Task Update(Stadium entity)
-    {
+        {
         throw new NotImplementedException();
-    }
+        }
 
     public Task<IEnumerable<Stadium>?> GetMatchByStadiumId(int Id)
-    {
+        {
         throw new NotImplementedException();
+        }
     }
-}
--- TicketVerkoop/Controllers/KalenderController.cs
+++ TicketVerkoop/Controllers/KalenderController.cs
@@ -8,59 +8,60 @@ using TicketVerkoop.Services.Interfaces;
 using TicketVerkoop.ViewModels;
 
 namespace TicketVerkoop.Controllers
-{
-    public class KalenderController : Controller
     {
+    public class KalenderController : Controller
+        {
+
         private IService<Match> matchService;
         private IService<Stadium> stadiumService;
         private readonly IMapper mapper;
 
-        public KalenderController(IService<Match> matchService, 
+        public KalenderController(IService<Match> matchService,
             IService<Stadium> stadiumService,
             IMapper mapper)
-        {
+            {
             this.matchService = matchService;
             this.stadiumService = stadiumService;
             this.mapper = mapper;
-        }
+            }
 
         public async Task<IActionResult> Index()
-        {
+            {
             ViewBag.lstStadiums = new SelectList(await stadiumService.GetAll(), "StadiumId", "Naam");
-            try 
-            {
-            var list = await matchService.GetAll();
-            List<MatchVM> listVM = mapper.Map<List<MatchVM>>(list);
-            return View(listVM);
-            }
+            try
+                {
+                var list = await matchService.GetAll();
+                List<MatchVM> listVM = mapper.Map<List<MatchVM>>(list);
+                return View(listVM);
+                }
             catch (Exception ex)
-            {
+                {
                 Debug.WriteLine("Errorlog " + ex.Message);
-            }
+                }
 
             return View();
-        }
+            }
 
         [HttpPost]
         public async Task<IActionResult> Index(int? StadiumId)
-        {
-            if (StadiumId == null)
             {
+            if (StadiumId == null)
+                {
                 return await Index();
-            }
+                }
 
             try
-            {
+                {
                 var list = await matchService.GetMatchByStadiumId(Convert.ToInt16(StadiumId));
                 ViewBag.lstStadiums = new SelectList(await stadiumService.GetAll(), "StadiumId", "Naam", StadiumId);
                 List<MatchVM> listVM = mapper.Map<List<MatchVM>>(list);
                 return View(listVM);
-            }
+                }
             catch (Exception ex)
-            {
+                {
                 Debug.WriteLine("Errorlog " + ex.Message);
-            }
+                }
             return View();
+            }
         }
     }
-}
