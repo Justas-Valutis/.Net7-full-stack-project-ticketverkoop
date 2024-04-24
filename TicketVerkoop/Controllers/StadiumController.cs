58bb0306bfdedb7f214bacaa25877ecc48da3584
Justas Valutis <r0940172@student.vives.be>
2024-04-24 02:40:26 +0200

    info stadium bekijken

---
+++ TicketVerkoop/Controllers/StadiumController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TicketVerkoop.Domains.Entities;
+using TicketVerkoop.Services.Interfaces;
+using TicketVerkoop.ViewModels;
+
+namespace TicketVerkoop.Controllers
+{
+    public class StadiumController : Controller
+    {
+        private IService<Stadium> stadiumService;
+        private readonly IMapper mapper;
+
+        public StadiumController(IService<Stadium> stadiumService,
+            IMapper mapper)
+        {
+            this.stadiumService = stadiumService;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> Index(int stadiumID)
+        {
+            var stadium = await stadiumService.FindById(Convert.ToInt16(stadiumID));
+            StadiumVM stadiumVM = mapper.Map<StadiumVM>(stadium);
+            return View(stadiumVM);
+        }
+    }
+}
