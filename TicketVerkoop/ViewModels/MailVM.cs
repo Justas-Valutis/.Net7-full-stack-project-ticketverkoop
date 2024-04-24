978bf48aa502aeffa206001472df692e06494b23
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-04-24 09:31:14 +0200

    Aanmaken MailVM en extra Class Library

---
+++ TicketVerkoop/ViewModels/MailVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketVerkoop.ViewModels
+{
+    public class MailVM
+    {
+        [Required, Display(Name = "Jouw naam")]
+        public string? FromName { get; set; }
+
+        [Required, Display(Name = "Jouw email")]
+        public string? FromEmail { get; set; }
+    }
+}
