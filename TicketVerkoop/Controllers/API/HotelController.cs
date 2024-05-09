2bcca2ec703d9971f604cb7857c59342c0378f78
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-09 11:21:31 +0200

    Begin Hotel Api

---
+++ TicketVerkoop/Controllers/API/HotelController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketVerkoop.Domains.Entities;
+using TicketVerkoop.ViewModels;
+
+namespace TicketVerkoop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : Controller
+    {
+
+        private readonly Stadium Stadium;
+
+        public HotelController()
+        {
+            Stadium = new Stadium();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetHotels()
+        {
+            var hotels = new[]
+            {
+            new HotelVM { Id = 1, Name = "Hotel ABC", Address = "123 Main St", City = Stadium.Stad.ToString(), Country = "België" },
+            new HotelVM { Id = 2, Name = "Hotel DEF", Address = "456 Elm St", City = Stadium.Stad.ToString(), Country = "België" },
+            
+        };
+            return Ok(hotels);
+        }
+    }
+}
